3e5eed04c1eb258285719d1c84b0dee2976cb619
alfer91 <dasungwkd@naver.com>
2021-10-07 18:51:49 +0900

    페이징 처리 & N+1 문제 & @EntityGraph 불완전한 처리 & @Query

--- src/test/java/org/zerock/bimovie/repository/MovieRepositoryTest.java
+++ src/test/java/org/zerock/bimovie/repository/MovieRepositoryTest.java
@@ -4,11 +4,18 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Commit;
 import org.zerock.bimovie.entity.Movie;
 import org.zerock.bimovie.entity.Poster;
 
 import javax.transaction.Transactional;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -46,7 +53,7 @@ class MovieRepositoryTest {
         movieRepository.save(movie);
     }*/
 
-    @Test
+    /*@Test
     @Transactional
     @Commit
     public void testRemovePoster() {
@@ -56,5 +63,63 @@ class MovieRepositoryTest {
         movie.removePoster(2L);
 
         movieRepository.save(movie);
+    }*/
+
+    /*@Test
+    public void insertMovies() {
+
+        IntStream.rangeClosed(10,100).forEach(i -> { //10부터 100까지 91개
+            Movie movie = Movie.builder().title("세계명작" + i).build();
+
+            movie.addPoster(Poster.builder().fname("세계명작" + i + "포스터1.jpg").build());
+            movie.addPoster(Poster.builder().fname("세계명작" + i + "포스터2.jpg").build());
+
+            movieRepository.save(movie);
+        });
+    }*/
+
+    /*@Test
+    public void testPaging1() {
+
+        Pageable pageable = PageRequest.of(1, 10, Sort.by("mno").descending());
+
+        Page<Movie> result = movieRepository.findAll(pageable);
+
+        result.getContent().forEach(m -> {
+            log.info(m.getMno());
+            log.info(m.getTitle());
+            log.info(m.getPosterList().size());
+            log.info("--------------------------");
+        });
+    }*/
+
+    /*@Test
+    public void testPaging2All() {
+
+        //문제가 발생했다.. limit가 없다.
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("mno").descending());
+
+        Page<Movie> result = movieRepository.findAll2(pageable);
+
+        result.getContent().forEach(m -> {
+            log.info(m.getMno());
+            log.info(m.getTitle());
+            log.info(m.getPosterList());
+            log.info("------------------------");
+        });
+    }*/
+
+    @Test
+    public void testPaging3All() {
+
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("mno").descending());
+
+        Page<Object[]> result = movieRepository.findAll3(pageable);
+
+        result.getContent().forEach(arr -> {
+            log.info(Arrays.toString(arr));
+
+            log.info("---------------------");
+        });
     }
 }
