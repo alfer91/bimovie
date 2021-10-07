09c9a89c65b03d7371d7a4cd63e245cf6e3a6906
alfer91 <dasungwkd@naver.com>
2021-10-07 17:59:44 +0900

    Movie에 Poster를 추가하는 경우

--- src/test/java/org/zerock/bimovie/repository/MovieRepositoryTest.java
+++ src/test/java/org/zerock/bimovie/repository/MovieRepositoryTest.java
@@ -4,8 +4,11 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.zerock.bimovie.entity.Movie;
 import org.zerock.bimovie.entity.Poster;
+
+import javax.transaction.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,7 +19,7 @@ class MovieRepositoryTest {
     @Autowired
     private MovieRepository movieRepository;
 
-    @Test
+    /*@Test
     public void testInsert() {
 
         log.info("testInsert...................");
@@ -29,5 +32,17 @@ class MovieRepositoryTest {
         movieRepository.save(movie);
 
         log.info(movie.getMno());
+    }*/
+
+    @Test
+    @Transactional
+    @Commit
+    public void testAddPoster() {
+
+        Movie movie = movieRepository.getOne(1L); //데이터베이스에 존재하는 영화 번호
+
+        movie.addPoster(Poster.builder().fname("극한직업포스터3.jpg").build()); //새로운 Poster 객체
+
+        movieRepository.save(movie);
     }
 }
